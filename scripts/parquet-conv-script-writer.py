4687523bd543d4295e93c8103cf722f07019c52e
adammichalsky <50460175+adammichalsky@users.noreply.github.com>
2019-07-31 13:45:04 -0400

    Updated due to dependency refactoring

--- scripts/parquet-conv-script-writer.py
+++ scripts/parquet-conv-script-writer.py
@@ -2,26 +2,26 @@
 import s3utils, awsutils, ec2utils, mskutils, listutils, os
 
 #Determine landing bucket and warehouse bucket
-dest_bucket_base = ec2utils.get'-bigdata-warehousing-'
+dest_bucket_base = '-bigdata-warehousing-'
 src_bucket_base = '-bigdata-landing-'
 dest_bucket = ec2utils.get_tag_value('Tenant Name') + dest_bucket_base + ec2utils.get_tag_value('Environment')
 src_bucket = ec2utils.get_tag_value('Tenant Name') + src_bucket_base + ec2utils.get_tag_value('Environment')
 
 #List unique folders in src bucket
-dest_list =  get_s3_folders(startAfter="",bucket=dest_bucket)
-src_list = get_s3_folders(startAfter="",bucket=src_bucket)
+dest_list =  s3utils.get_s3_folders(startAfter="",bucket=dest_bucket)
+src_list = s3utils.get_s3_folders(startAfter="",bucket=src_bucket)
 
-dest_list = deduplicate_list(dest_list)
-src_list = deduplicate_list(src_list)
+dest_list = listutils.deduplicate_list(dest_list)
+src_list = listutils.deduplicate_list(src_list)
 
-delta_list = diff(src_list,dest_list)
+delta_list = listutils.diff_list(src_list,dest_list)
 
 
 with open('/tmp/orc-to-parquet.py', 'w+') as file:
     file.write("#!/usr/bin/python \n")
     file.write("from __future__ import print_function \n" + "import sys \n" + "from pyspark.sql import SparkSession,functions as F \n")
     file.write("spark = SparkSession.builder.appName(\"initialization\").getOrCreate()\n\n")
-    for df in range(0,len(src_list))-1):
+    for df in range(0,len(src_list)-1):
         if src_list[df] in delta_list:
             file.write("base = spark.read.format(\"orc\").load(\"s3://" + src_bucket + "/" + src_list[df] + "/\")\n")
             file.write("newBase = base.withColumn(\"ProcessingTime\", F.current_timestamp())\n")
